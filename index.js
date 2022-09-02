7a25f9e1f6b6d08f2337835c82fb41e9920eaf97
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-09-02 14:22:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    const text = document.querySelector('#text')
+    return text.innerHTML = "This is really cool!"
+});
